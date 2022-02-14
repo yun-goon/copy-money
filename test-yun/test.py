f058b53623e2a6fe047f7ccb083d9c56da050f10
yun-goon <sirius584@naver.com>
2022-02-15 00:17:35 +0900

    보조지표 저장

--- test-yun/test.py
+++ test-yun/test.py
@@ -22,46 +22,52 @@ if __name__ == '__main__':
 
     # noinspection PyBroadException
     try:
+        #while 1:
 
-        upbit.set_loglevel('I')
+            upbit.set_loglevel('I')
 
-        indicators = upbit.get_indicator_sel('KRW-BTC', '60', 200, 1, ['RSI', 'MFI', 'MACD', 'WILLIAMS'])
+            indicators = upbit.get_indicator_sel('KRW-BTC', 'D', 200, 100, ['RSI', 'MFI', 'MACD', 'WILLIAMS'])
 
-        # 보조지표 추출
-        rsi_data = indicators['RSI']
-        mfi_data = indicators['MFI']
-        macd_data = indicators['MACD']
-        williams_data = indicators['WILLIAMS']
+            # 보조지표 추출
+            rsi_data = indicators['RSI']
+            mfi_data = indicators['MFI']
+            macd_data = indicators['MACD']
+            williams_data = indicators['WILLIAMS']
 
-        logging.info(rsi_data)
-        logging.info(mfi_data)
-        print(macd_data)
-        print(williams_data)
+            # logging.info(rsi_data)
+            #logging.info(mfi_data)
+            # print(macd_data)
+            # print(williams_data)
+            for i in range(0,100):
 
-        rsi = rsi_data[0]['RSI']
-        mfi = mfi_data[0]['MFI']
-        macd= macd_data[0]['MACD']
-        r = williams_data[0]['W']
-        now = datetime.datetime.now()
+                dt = rsi_data[i]['DT']
+                rsi = rsi_data[i]['RSI']
+                mfi = mfi_data[i]['MFI']
+                macd= macd_data[i]['MACD']
+                r = williams_data[i]['W']
+                now = datetime.datetime.now()
 
 
-        list_index = ['TIME','RSI','MFI','MACD','R%']
-        list =[]
-        line=[]
-        line.append(now)
-        line.append(rsi)
-        line.append(mfi)
-        line.append(macd)
-        line.append(r)
+                list_index = ['TIME','RSI','MFI','MACD','R%']
+                list =[]
+                line=[]
+                line.append(dt)
+                line.append(rsi)
+                line.append(mfi)
+                line.append(macd)
+                line.append(r)
 
-        list.append(line)
+                list.append(line)
 
-        df=pd.DataFrame(list, columns=list_index)
+                df=pd.DataFrame(list, columns=list_index)
 
-        if not os.path.exists('indicator.csv'):
-            df.to_csv('indicator.csv', index=False, mode='w', encoding='utf-8-sig')
-        else:
-            df.to_csv('indicator.csv', index=False, mode='a', encoding='utf-8-sig', header=False)
+                if not os.path.exists('indicator.csv'):
+                    df.to_csv('indicator.csv', index=False, mode='w', encoding='utf-8-sig')
+                else:
+                    df.to_csv('indicator.csv', index=False, mode='a', encoding='utf-8-sig', header=False)
+
+
+            # time.sleep(60)
 
 
 
