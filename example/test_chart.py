c8f93b0c4f0dac9b3e753ab2d5458fa59f8ca006
종하 <72875357+hytric@users.noreply.github.com>
2022-02-18 01:52:31 +0900

    chart testing

---
+++ example/test_chart.py
@@ -0,0 +1,33 @@
+import json
+import requests
+import plotly.graph_objects as go
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+# coin 차트 데이터 불러오기
+from pandas.io.json import json_normalize
+
+headers = {"Accept": "application/json"}
+
+response = requests.request("GET", "https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=100", headers=headers)
+
+print(response.json())
+df = json_normalize(response.json())  # Results contain the required data
+print(df)
+
+stock_name = '비트코인'
+
+fig = go.Figure(data=[go.Candlestick(x=df['candle_date_time_kst'],
+                                    open=df['opening_price'],
+                                    high=df['high_price'],
+                                    low=df['low_price'],
+                                    close=df['trade_price'])])
+# x축 type을 카테고리 형으로 설정, 순서를 오름차순으로 날짜순서가 되도록 설정
+fig.layout = dict(title=stock_name,
+                       xaxis = dict(type="category",
+                                    categoryorder='category ascending'))
+fig.update_xaxes(nticks=5)
+fig.show()
